4f64074e8b6f5d8cca3dafd942053ab8686c1ea9
splafi <martin_petkov555@abv.bg>
2016-10-23 07:45:37 +0300

    Exam

---
+++ Exam Preparetion/Array Manipulator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions
+                .RemoveEmptyEntries)
+                .ToList();
+            string[] command = Console.ReadLine().Split();
+
+
+            while (!command[0].Equals("end"))
+            {
+                switch (command[0])
+                {
+                    case "reverse":
+                        int start = int.Parse(command[2]);
+                        int count = int.Parse(command[2]);
+                        List<string> currList = input
+                                            .Skip(start)
+                                            .Take(count)
+                                            .Reverse()
+                                            .ToList();
+                        input.RemoveRange(start, count);
+                        input.InsertRange(start, currList);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                command = Console.ReadLine().Split();
+            }
+
+            string output =  string.Join(", ", input);
+            Console.WriteLine($"[{output}]");
+        }
+    }
+}
---
+++ Exam Preparetion/Exam Preparetion/SweetDesert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_Preparetion
+{
+    class SweetDesert
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int guestNumber = int.Parse(Console.ReadLine());
+            double bannanaPrice = double.Parse(Console.ReadLine());
+            double eggsPrice = double.Parse(Console.ReadLine());
+            double berriesPrice = double.Parse(Console.ReadLine());
+            var portion = 2 * bannanaPrice + 4 * eggsPrice + 0.2 * berriesPrice;
+            var neededPortion =Math.Ceiling(guestNumber / 6.0);
+            var portionPrices = portion *neededPortion;
+            if (portionPrices>money)
+            {
+                Console.WriteLine("Ivancho will have to withdraw money - he will need {0:F2}lv more.", portionPrices - money);
+            }
+            else
+            {
+                Console.WriteLine("Ivancho has");
+            }
+        }
+    }
+}
